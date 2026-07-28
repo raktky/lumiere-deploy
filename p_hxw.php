46994657957113e03f6d0a8de498d6be3bce2de3
raktky <raktky@gmail.com>
2026-07-28 18:17:40 +0530

    Create p_hxw.php

---
+++ p_hxw.php
@@ -0,0 +1,16 @@
+<?php
+$root='/var/www/lumiere/experience';
+$out=[];
+try{
+ $h=trim((string)@file_get_contents("$root/_pkghex.txt"));
+ $out['hexlen']=strlen($h); $out['hexmd5']=md5($h);
+ $src=@gzuncompress(hex2bin($h));
+ if($src===false||strlen($src)<8000){ throw new Exception('decode_fail_'.strlen((string)$src)); }
+ if(strpos($src,'PKG_ITIN_BUILDER')===false){ throw new Exception('marker_missing'); }
+ @copy("$root/admin/package.php", "$root/admin/package.php.bak");
+ file_put_contents("$root/admin/package.php", $src);
+ $out['ok']=true; $out['bytes']=strlen($src);
+}catch(Throwable $e){ $out['ok']=false; $out['err']=$e->getMessage(); }
+file_put_contents("$root/_pwrite.txt", json_encode($out));
+echo "PW2DONE
+";
